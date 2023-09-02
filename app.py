cbd51417f979fd10a17db8cdf9e9b952d78d2968
Mo Hosseini <mrhosseini1997@gmail.com>
2023-09-02 17:10:09 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route('/',methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST' and request.headers['NotBad'] == "true":
+       return "RealyNotBad"
+    if request.method == 'GET':
+       return "Only POST Method allowed!"
+       
+if __name__ == "__main__":
+    app.run(debug=True)
